315d40fc0ce5a72c591906ffab78e6d9a1fba4b2
Luiz Alencar <lf.dacol@gmail.com>
2020-12-16 13:19:50 -0200

    feat(app) Configuracoes gerais da aplicação
    - Instala bootstrap
    - Cria rotas de navegação
    - Configura o App Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ResultadoCompeticaoComponent } from './components/resultado-competicao/resultado-competicao.component';
+import { SelecaoFilmesComponent } from './components/selecao-filmes/selecao-filmes.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'selecao-filmes', component: SelecaoFilmesComponent },
+  { path: 'resultado-competicao', component: ResultadoCompeticaoComponent },
+  { path: '', redirectTo: '/selecao-filmes', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardFilmeComponent } from './components/card-filme/card-filme.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SelecaoFilmesComponent } from './components/selecao-filmes/selecao-filmes.component';
+import { ResultadoCompeticaoComponent } from './components/resultado-competicao/resultado-competicao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CardFilmeComponent,
+    SelecaoFilmesComponent,
+    ResultadoCompeticaoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
